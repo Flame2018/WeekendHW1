0baa74b42f03bdc2cf0ea9a830d6fb5d0f227e7a
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-10-22 13:27:20 -0500

    Add files via upload

---
+++ Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WeekendHW.Data;
+
+namespace WeekendHW.Pages.myAssociation
+{
+    public class EditModel : PageModel
+    {
+        private readonly WeekendHW.Data.ApplicationDbContext _context;
+
+        public EditModel(WeekendHW.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public HAssociation HAssociation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            HAssociation = await _context.HAssociation.FirstOrDefaultAsync(m => m.HAID == id);
+
+            if (HAssociation == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(HAssociation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HAssociationExists(HAssociation.HAID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool HAssociationExists(int id)
+        {
+            return _context.HAssociation.Any(e => e.HAID == id);
+        }
+    }
+}
---
+++ Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WeekendHW.Data;
+
+
+namespace WeekendHW.Pages.myAssociation
+{
+    [Authorize(Roles = "headhunter")]
+    public class IndexModel : PageModel
+    {
+        private readonly WeekendHW.Data.ApplicationDbContext _context;
+
+        [BindProperty]
+        public int OpenCount { get; set; }
+
+        [BindProperty]
+        public int ClosedCount { get; set; }
+
+        public IndexModel(WeekendHW.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<HAssociation> HAssociation { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            HAssociation = await _context.HAssociation.ToListAsync();
+
+            foreach(var item in HAssociation)
+            {
+                if(item.ApprPos == false)
+                {
+                    OpenCount += 1;
+                }else if (item.ApprPos == false)
+                {
+                    ClosedCount += 1;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
